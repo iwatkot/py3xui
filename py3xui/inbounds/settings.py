3caae2e3b0142e9e14e41bb33b4c2d3c220055f2
iwatkot <iwatkot@gmail.com>
2024-06-18 00:59:22 +0200

    Pydantic validators.

--- py3xui/inbounds/settings.py
+++ py3xui/inbounds/settings.py
@@ -1,23 +1,3 @@
-# {
-#     "clients": [
-#         {
-#             "email": "alhtim2x",
-#             "enable": True,
-#             "expiryTime": 1718616488524,
-#             "flow": "",
-#             "id": "d76eb6ed-0697-4cd5-a8c5-8cfeb4d1b209",
-#             "limitIp": 0,
-#             "reset": 0,
-#             "subId": "21c2lku9iyjm7a0o",
-#             "tgId": "",
-#             "totalGB": 0,
-#         }
-#     ],
-#     "decryption": "none",
-#     "fallbacks": [],
-# }
-
-
 from pydantic import BaseModel, Field
 
 from py3xui.inbounds.bases import JsonStringModel
