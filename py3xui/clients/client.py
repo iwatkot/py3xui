59664e4d1c443524c87b66d432044aacb0ef8e31
iwatkot <iwatkot@gmail.com>
2024-06-18 18:40:57 +0200

    Merged models for clients.

--- py3xui/clients/client.py
+++ py3xui/clients/client.py
@@ -15,21 +15,34 @@ class ClientFields:
     TOTAL = "total"
     RESET = "reset"
 
+    FLOW = "flow"
+    LIMIT_IP = "limitIp"
+    SUB_ID = "subId"
+    TG_ID = "tgId"
+    TOTAL_GB = "totalGB"
+
 
 class Client(BaseModel):
-    id: int
-    inbound_id: int = Field(alias=ClientFields.INBOUND_ID)  # type: ignore
-    enable: bool
-    email: str
-    up: int
-    down: int
-    expiry_time: int = Field(alias=ClientFields.EXPIRY_TIME)  # type: ignore
-    total: int
-    reset: int
+    id: int | str | None = None
+    inbound_id: int = Field(default=None, alias=ClientFields.INBOUND_ID)  # type: ignore
+    enable: bool | None = None
+    email: str | None = None
+    up: int | None = None
+    down: int | None = None
+    expiry_time: int | None = Field(default=None, alias=ClientFields.EXPIRY_TIME)  # type: ignore
+    total: int | None = None
+    reset: int | None = None
+
+    flow: str | None = None
+    limit_ip: int | None = Field(default=None, alias=ClientFields.LIMIT_IP)  # type: ignore
+    sub_id: str | None = Field(default=None, alias=ClientFields.SUB_ID)  # type: ignore
+    tg_id: str | None = Field(default=None, alias=ClientFields.TG_ID)  # type: ignore
+    total_gb: int | None = Field(default=None, alias=ClientFields.TOTAL_GB)  # type: ignore
 
     def __repr__(self) -> str:
         return (
             f"Client(id={self.id}, inbound_id={self.inbound_id}, enable={self.enable}, "
             f"email={self.email}, up={self.up}, down={self.down}, expiry_time={self.expiry_time}, "
-            f"total={self.total}, reset={self.reset})"
+            f"total={self.total}, reset={self.reset}, flow={self.flow}, limit_ip={self.limit_ip}, "
+            f"sub_id={self.sub_id}, tg_id={self.tg_id}, total_gb={self.total_gb})"
         )
--- py3xui/inbounds/settings.py
+++ py3xui/inbounds/settings.py
@@ -1,22 +1,5 @@
-from pydantic import BaseModel, Field
-
+from py3xui.clients.client import Client
 from py3xui.inbounds.bases import JsonStringModel
-
-
-# pylint: disable=too-few-public-methods
-class SettingsClientFields:
-    """Stores the fields returned by the XUI API for parsing."""
-
-    EMAIL = "email"
-    ENABLE = "enable"
-    EXPIRY_TIME = "expiryTime"
-    FLOW = "flow"
-    ID = "id"
-    LIMIT_IP = "limitIp"
-    RESET = "reset"
-    SUB_ID = "subId"
-    TG_ID = "tgId"
-    TOTAL_GB = "totalGB"
 
 
 # pylint: disable=too-few-public-methods
@@ -28,29 +11,8 @@ class SettingsFields:
     FALLBACKS = "fallbacks"
 
 
-class SettingsClient(BaseModel):
-    email: str
-    enable: bool
-    expiry_time: int = Field(alias=SettingsClientFields.EXPIRY_TIME)  # type: ignore
-    flow: str
-    id: str
-    limit_ip: int = Field(alias=SettingsClientFields.LIMIT_IP)  # type: ignore
-    reset: int
-    sub_id: str = Field(alias=SettingsClientFields.SUB_ID)  # type: ignore
-    tg_id: str = Field(alias=SettingsClientFields.TG_ID)  # type: ignore
-    total_gb: int = Field(alias=SettingsClientFields.TOTAL_GB)  # type: ignore
-
-    def __repr__(self) -> str:
-        return (
-            f"SettingsClient(email={self.email}, enable={self.enable}, "
-            f"expiry_time={self.expiry_time}, "
-            f"flow={self.flow}, id={self.id}, limit_ip={self.limit_ip}, reset={self.reset}, "
-            f"sub_id={self.sub_id}, tg_id={self.tg_id}, total_gb={self.total_gb})"
-        )
-
-
 class Settings(JsonStringModel):
-    clients: list[SettingsClient]
+    clients: list[Client]
     decryption: str
     fallbacks: list
 
