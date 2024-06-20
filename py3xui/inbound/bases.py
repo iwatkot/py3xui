013cebb73013194566bb62d9701cbe683e434ba3
iwatkot <iwatkot@gmail.com>
2024-06-20 03:15:46 +0200

    Pylint action.

--- py3xui/inbound/bases.py
+++ py3xui/inbound/bases.py
@@ -5,6 +5,7 @@ import json
 from pydantic import BaseModel, model_validator
 
 
+# pylint: disable=too-few-public-methods
 class JsonStringModel(BaseModel):
     """Base class for models that have a JSON string as a field."""
 
@@ -13,6 +14,14 @@ class JsonStringModel(BaseModel):
         cls,
         values,
     ):  # pylint: disable=no-self-argument, arguments-differ
+        """Converts the JSON string to a dictionary if it is a string.
+
+        Args:
+            values (Any): The values to validate.
+
+        Returns:
+            Any: The validated values.
+        """
         if isinstance(values, str):
             try:
                 return json.loads(values)
