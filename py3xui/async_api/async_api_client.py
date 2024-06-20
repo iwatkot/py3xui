0719c70347a32591e3eae8466ba76e507aeb2f30
iwatkot <iwatkot@gmail.com>
2024-06-20 23:28:53 +0200

    Async client api implemented.

--- py3xui/async_api/async_api_client.py
+++ py3xui/async_api/async_api_client.py
@@ -1,6 +1,9 @@
 """This module contains the ClientApi class which provides methods to interact with the
 clients in the XUI API."""
 
+import json
+from typing import Any
+
 from py3xui.api.api_base import ApiFields
 from py3xui.async_api.async_api_base import AsyncBaseApi
 from py3xui.client.client import Client
@@ -37,6 +40,8 @@ class AsyncClientApi(AsyncBaseApi):
 
         api = py3xui.AsyncApi.from_env()
 
+        api.login()
+
         client: py3xui.Client = api.client.get_by_email("email")
 
         new_client = py3xui.Client(id=new_uuid, email="test", enable=True)
@@ -63,6 +68,7 @@ class AsyncClientApi(AsyncBaseApi):
             import py3xui
 
             api = py3xui.AsyncApi.from_env()
+            api.login()
             client: py3xui.Client = await api.client.get_by_email("email")
             ```
         """  # pylint: disable=line-too-long
@@ -80,3 +86,259 @@ class AsyncClientApi(AsyncBaseApi):
             logger.warning("No client found for email: %s", email)
             return None
         return Client.model_validate(client_json)
+
+    async def get_ips(self, email: str) -> list[str]:
+        """This route is used to retrieve the IP records associated with a specific client
+        identified by their email.
+
+        [Source documentation](https://documenter.getpostman.com/view/16802678/2s9YkgD5jm#06f1214c-dbb0-49f2-81b5-8e924abd19a9)
+
+        Arguments:
+            email (str): The email of the client to retrieve.
+
+        Returns:
+            list[str]: The list of IP records associated with the client.
+
+        Examples:
+            ```python
+            import py3xui
+
+            api = py3xui.AsyncApi.from_env()
+            api.login()
+            ips = await api.client.get_ips("email")
+            ```
+        """  # pylint: disable=line-too-long
+        endpoint = f"panel/api/inbounds/clientIps/{email}"
+        headers = {"Accept": "application/json"}
+
+        url = self._url(endpoint)
+        logger.info("Getting client IPs for email: %s", email)
+
+        response = await self._post(url, headers, {})
+
+        ips_json = response.json().get(ApiFields.OBJ)
+        return ips_json if ips_json != ApiFields.NO_IP_RECORD else []
+
+    async def add(self, inbound_id: int, clients: list[Client]):
+        """This route is used to add a new clients to a specific inbound identified by its ID.
+
+        [Source documentation](https://documenter.getpostman.com/view/16802678/2s9YkgD5jm#c4d325ae-fbb4-44e9-bd2e-29eebb7fbc52)
+
+        Arguments:
+            inbound_id (int): The ID of the inbound to add the clients to.
+            clients (list[Client]): The list of clients to add.
+
+        Examples:
+            ```python
+            import uuid
+            import py3xui
+
+            api = py3xui.AsyncApi.from_env()
+            api.login()
+
+            new_client = py3xui.Client(id=str(uuid.uuid4()), email="test", enable=True)
+            inbound_id = 1
+
+            api.client.add(inbound_id, [new_client])
+        """  # pylint: disable=line-too-long
+        endpoint = "panel/api/inbounds/addClient"
+        headers = {"Accept": "application/json"}
+
+        url = self._url(endpoint)
+        settings = {
+            "clients": [
+                client.model_dump(by_alias=True, exclude_defaults=True) for client in clients
+            ]
+        }
+        data = {"id": inbound_id, "settings": json.dumps(settings)}
+        logger.info("Adding %s clients to inbound with ID: %s", len(clients), inbound_id)
+
+        await self._post(url, headers, data)
+        logger.info("Client added successfully.")
+
+    async def update(self, client_uuid: str, client: Client) -> None:
+        """This route is used to update an existing client identified by its UUID within a specific
+        inbound.
+
+        [Source documentation](https://documenter.getpostman.com/view/16802678/2s9YkgD5jm#af1bee51-199c-4176-b3ab-d325ba2fae19)
+
+        Arguments:
+            client_uuid (str): The UUID of the client to update.
+            client (Client): The client object with updated information.
+
+        Examples:
+            ```python
+            import py3xui
+
+            api = py3xui.AsyncApi.from_env()
+            api.login()
+            client = api.client.get_by_email("email")
+            client.email = "newemail"
+            api.client.update(client.id, client)
+            ```
+        """  # pylint: disable=line-too-long
+        endpoint = f"panel/api/inbounds/updateClient/{client_uuid}"
+        headers = {"Accept": "application/json"}
+
+        url = self._url(endpoint)
+        settings = {"clients": [client.model_dump(by_alias=True, exclude_defaults=True)]}
+        data = {"id": client.inbound_id, "settings": json.dumps(settings)}
+
+        logger.info("Updating client: %s", client)
+        await self._post(url, headers, data)
+        logger.info("Client updated successfully.")
+
+    async def reset_ips(self, email: str) -> None:
+        """This route is used to reset or clear the IP records associated with a specific client
+        identified by their email address.
+
+        [Source documentation](https://documenter.getpostman.com/view/16802678/2s9YkgD5jm#7af93bc4-693a-4fa4-8560-0642783af6f3)
+
+        Arguments:
+            email (str): The email of the client to reset the IPs for.
+
+        Examples:
+            ```python
+            import py3xui
+
+            api = py3xui.AsyncApi.from_env()
+            api.login()
+
+            api.client.reset_ips("email")
+            ```
+        """  # pylint: disable=line-too-long
+        endpoint = f"panel/api/inbounds/clearClientIps/{email}"
+        headers = {"Accept": "application/json"}
+
+        url = self._url(endpoint)
+        data: dict[str, Any] = {}
+        logger.info("Resetting client IPs for email: %s", email)
+
+        await self._post(url, headers, data)
+        logger.info("Client IPs reset successfully.")
+
+    async def reset_stats(self, inbound_id: int, email: str) -> None:
+        """This route is used to reset the traffic statistics for a specific client identified by
+        their email address  within a particular inbound identified by its ID.
+
+        [Source documentation](https://documenter.getpostman.com/view/16802678/2s9YkgD5jm#52081826-8e06-4dc1-9bad-8a95f1cd8a96)
+
+        Arguments:
+            inbound_id (int): The ID of the inbound to reset the client stats.
+            email (str): The email of the client to reset the stats for.
+
+        Examples:
+            ```python
+            import py3xui
+
+            api = py3xui.AsyncApi.from_env()
+            api.login()
+            inbound_id = 1
+
+            api.client.reset_stats(inbound_id, "test")
+            ```
+        """  # pylint: disable=line-too-long
+        endpoint = f"panel/api/inbounds/{inbound_id}/resetClientTraffic/{email}"
+        headers = {"Accept": "application/json"}
+
+        url = self._url(endpoint)
+        data: dict[str, Any] = {}
+        logger.info("Resetting client stats for inbound ID: %s, email: %s", inbound_id, email)
+
+        await self._post(url, headers, data)
+        logger.info("Client stats reset successfully.")
+
+    async def delete(self, inbound_id: int, client_uuid: str) -> None:
+        """This route is used to delete a client identified by its UUID within a specific inbound
+        identified by its ID.
+
+        [Source documentation](https://documenter.getpostman.com/view/16802678/2s9YkgD5jm#97aa9d0e-9cc3-46db-a364-c2fda39586bd)
+
+        Arguments:
+            inbound_id (int): The ID of the inbound to delete the client from.
+            client_uuid (str): The UUID of the client to delete.
+
+        Examples:
+            ```python
+            import py3xui
+
+            api = py3xui.AsyncApi.from_env()
+            api.login()
+            client = api.client.get_by_email("email")
+            inbound_id = 1
+
+            api.client.delete(inbound_id, client.id)
+            ```
+        """  # pylint: disable=line-too-long
+        endpoint = f"panel/api/inbounds/{inbound_id}/delClient/{client_uuid}"
+        headers = {"Accept": "application/json"}
+
+        url = self._url(endpoint)
+        data: dict[str, Any] = {}
+        logger.info("Deleting client with ID: %s", client_uuid)
+
+        await self._post(url, headers, data)
+        logger.info("Client deleted successfully.")
+
+    async def delete_depleted(self, inbound_id: int) -> None:
+        """This route is used to delete all depleted clients associated with a specific inbound
+        identified by its ID.
+
+        [Source documentation](https://documenter.getpostman.com/view/16802678/2s9YkgD5jm#8f4975c9-1051-43cb-afa7-c42ca2542c6b)
+
+        Arguments:
+            inbound_id (int): The ID of the inbound to delete the depleted clients from.
+
+        Examples:
+
+            ```python
+            import py3xui
+
+            api = py3xui.AsyncApi.from_env()
+            api.login()
+
+            inbounds: list[py3xui.Inbound] = api.inbound.get_list()
+
+            for inbound in inbounds:
+                api.client.delete_depleted(inbound.id)
+            ```
+        """  # pylint: disable=line-too-long
+        endpoint = f"panel/api/inbounds/delDepletedClients/{inbound_id}"
+        headers = {"Accept": "application/json"}
+
+        url = self._url(endpoint)
+        data: dict[str, Any] = {}
+        logger.info("Deleting depleted clients for inbound ID: %s", inbound_id)
+
+        await self._post(url, headers, data)
+        logger.info("Depleted clients deleted successfully.")
+
+    async def online(self) -> list[str]:
+        """Returns a list of email addresses of online clients.
+
+        [Source documentation](https://documenter.getpostman.com/view/16802678/2s9YkgD5jm#9cac8101-017e-4415-94e2-d30f4dcf49de)
+
+        Returns:
+            list[str]: The list of email addresses of online clients.
+
+        Examples:
+            ```python
+            import py3xui
+
+            api = py3xui.AsyncApi.from_env()
+            api.login()
+            res = api.client.online()
+            print(res)
+            ```
+
+        """  # pylint: disable=line-too-long
+        endpoint = "panel/api/inbounds/onlines"
+        headers = {"Accept": "application/json"}
+
+        url = self._url(endpoint)
+        data: dict[str, Any] = {}
+        logger.info("Getting online clients")
+
+        response = await self._post(url, headers, data)
+        online = response.json().get(ApiFields.OBJ)
+        return online or []
--- tests/test_async_api.py
+++ tests/test_async_api.py
@@ -1,17 +1,11 @@
 import json
 import os
 import uuid
-from unittest import mock
 
-import httpx
 import pytest
 import respx
-from aiohttp import ClientResponse
-from aioresponses import aioresponses
 
-from py3xui import AsyncApi, Client, Inbound
-from py3xui.api.api_base import ApiFields
-from py3xui.inbound import Settings, Sniffing, StreamSettings
+from py3xui import AsyncApi, Client
 
 RESPONSES_DIR = "tests/responses"
 HOST = "http://localhost"
@@ -36,3 +30,104 @@ async def test_get_client():
         assert isinstance(client, Client), f"Expected Client, got {type(client)}"
         assert client.email == EMAIL, f"Expected {EMAIL}, got {client.email}"
         assert client.id == 1, f"Expected 1, got {client.id}"
+
+
+@pytest.mark.asyncio
+async def test_get_ips():
+    response_example = {"success": True, "msg": "", "obj": "No IP Record"}
+
+    with respx.mock:
+        request = respx.post(f"{HOST}/panel/api/inbounds/clientIps/{EMAIL}").respond(
+            200, json=response_example
+        )
+        api = AsyncApi(HOST, USERNAME, PASSWORD)
+        ips = await api.client.get_ips(EMAIL)
+
+        assert request.called, "Mocked request was not called"
+        assert ips == [], f"Expected None, got {ips}"
+
+
+@pytest.mark.asyncio
+async def test_add_clients():
+    client = Client(id=str(uuid.uuid4()), email="test", enable=True)
+    with respx.mock:
+        request = respx.post(f"{HOST}/panel/api/inbounds/addClient").respond(
+            200, json={"success": True}
+        )
+        api = AsyncApi(HOST, USERNAME, PASSWORD)
+        await api.client.add(1, [client])
+
+        assert request.called, "Mocked request was not called"
+
+
+@pytest.mark.asyncio
+async def test_update_client():
+    client = Client(id=str(uuid.uuid4()), email="test", enable=True)
+    with respx.mock:
+        request = respx.post(f"{HOST}/panel/api/inbounds/updateClient/{client.id}").respond(
+            200, json={"success": True}
+        )
+        api = AsyncApi(HOST, USERNAME, PASSWORD)
+        await api.client.update(client.id, client)
+
+        assert request.called, "Mocked request was not called"
+
+
+@pytest.mark.asyncio
+async def test_reset_client_ips():
+    with respx.mock:
+        request = respx.post(f"{HOST}/panel/api/inbounds/clearClientIps/{EMAIL}").respond(
+            200, json={"success": True}
+        )
+        api = AsyncApi(HOST, USERNAME, PASSWORD)
+        await api.client.reset_ips(EMAIL)
+
+        assert request.called, "Mocked request was not called"
+
+
+@pytest.mark.asyncio
+async def test_reset_client_stats():
+    with respx.mock:
+        request = respx.post(f"{HOST}/panel/api/inbounds/1/resetClientTraffic/{EMAIL}").respond(
+            200, json={"success": True}
+        )
+        api = AsyncApi(HOST, USERNAME, PASSWORD)
+        await api.client.reset_stats(1, EMAIL)
+
+        assert request.called, "Mocked request was not called"
+
+
+@pytest.mark.asyncio
+async def test_delete_client():
+    with respx.mock:
+        request = respx.post(f"{HOST}/panel/api/inbounds/1/delClient/1").respond(
+            200, json={"success": True}
+        )
+        api = AsyncApi(HOST, USERNAME, PASSWORD)
+        await api.client.delete(1, "1")
+
+        assert request.called, "Mocked request was not called"
+
+
+@pytest.mark.asyncio
+async def test_delete_depleted_clients():
+    with respx.mock:
+        request = respx.post(f"{HOST}/panel/api/inbounds/delDepletedClients/1").respond(
+            200, json={"success": True}
+        )
+        api = AsyncApi(HOST, USERNAME, PASSWORD)
+        await api.client.delete_depleted(1)
+
+        assert request.called, "Mocked request was not called"
+
+
+@pytest.mark.asyncio
+async def test_client_online():
+    with respx.mock:
+        request = respx.post(f"{HOST}/panel/api/inbounds/onlines").respond(
+            200, json={"success": True}
+        )
+        api = AsyncApi(HOST, USERNAME, PASSWORD)
+        await api.client.online()
+
+        assert request.called, "Mocked request was not called"
