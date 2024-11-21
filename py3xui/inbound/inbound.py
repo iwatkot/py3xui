2e77e5632eadfcb3f34ed4b0a7270817deb7a497
iwatkot <iwatkot@gmail.com>
2024-11-21 20:09:40 +0100

    StreamSettings no deserialization from JSON string fix.

--- py3xui/inbound/inbound.py
+++ py3xui/inbound/inbound.py
@@ -1,8 +1,9 @@
 """This module contains the Inbound class, which represents an inbound connection in the XUI API."""
 
+import json
 from typing import Any
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 from py3xui.client.client import Client
 from py3xui.inbound.settings import Settings
@@ -84,6 +85,32 @@ class Inbound(BaseModel):
         populate_by_name=True,
     )
 
+    @field_validator("stream_settings")
+    def validate_stream_settings(  # pylint: disable=no-self-argument
+        cls, value: StreamSettings | str
+    ) -> StreamSettings | str:
+        """Validates the stream settings field taking into account that it can be either a
+        dictionary with the StreamSettings model, JSON string or an empty string.
+        If the value is a string, it will try to parse it as JSON and create a StreamSettings,
+        otherwise it will return the value as is (empty string).
+
+        Args:
+            value (StreamSettings | str): The value to validate.
+
+        Returns:
+            StreamSettings | str: The validated value.
+        """
+        if isinstance(value, dict):
+            return StreamSettings(**value)
+        if isinstance(value, str):
+            try:
+                data = json.loads(value)
+                return StreamSettings(**data)
+            except json.JSONDecodeError:
+                pass
+        return value
+
+    # pylint: disable=no-member, no-self-argument
     def to_json(self) -> dict[str, Any]:
         """Converts the Inbound instance to a JSON-compatible dictionary for the XUI API.
 
@@ -109,7 +136,7 @@ class Inbound(BaseModel):
             }
         )
 
-        # Handle stream_settings which can be either StreamSettings or str
+        # Handle stream_settings which can be either StreamSettings or str.
         if isinstance(self.stream_settings, StreamSettings):
             result[InboundFields.STREAM_SETTINGS] = self.stream_settings.model_dump_json(
                 by_alias=True
