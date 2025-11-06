76e2e77175bcc31d40032053e401e2ddf5d58054
ladev <aleksei.lapochkin1@gmail.com>
2025-11-06 19:11:15 +0300

    Added uuid to serialize

--- py3xui/inbound/inbound.py
+++ py3xui/inbound/inbound.py
@@ -1,147 +1,81 @@
-"""This module contains the Inbound class, which represents an inbound connection in the XUI API."""
+"""This module contains the Client class which represents a client in the XUI API."""
 
-import json
-from typing import Any
-
-from pydantic import BaseModel, ConfigDict, Field, field_validator
-
-from py3xui.client.client import Client
-from py3xui.inbound.settings import Settings
-from py3xui.inbound.sniffing import Sniffing
-from py3xui.inbound.stream_settings import StreamSettings
+from pydantic import BaseModel, ConfigDict, Field
 
 
 # pylint: disable=too-few-public-methods
-class InboundFields:
+class ClientFields:
     """Stores the fields returned by the XUI API for parsing."""
 
+    EMAIL = "email"
     ENABLE = "enable"
-    PORT = "port"
-    PROTOCOL = "protocol"
-    SETTINGS = "settings"
-    STREAM_SETTINGS = "streamSettings"
-    SNIFFING = "sniffing"
+    PASSWORD = "password"
 
     ID = "id"
+    INBOUND_ID = "inboundId"
+
     UP = "up"
     DOWN = "down"
-    TOTAL = "total"
-    REMARK = "remark"
 
     EXPIRY_TIME = "expiryTime"
-    CLIENT_STATS = "clientStats"
-    LISTEN = "listen"
 
-    TAG = "tag"
+    TOTAL = "total"
+    RESET = "reset"
+
+    FLOW = "flow"
+    METHOD = "method"
+    LIMIT_IP = "limitIp"
+    SUB_ID = "subId"
+    TG_ID = "tgId"
+    TOTAL_GB = "totalGB"
 
 
-class Inbound(BaseModel):
-    """Represents an inbound connection in the XUI API.
+class Client(BaseModel):
+    """Represents a client in the XUI API.
 
     Attributes:
-        enable (bool): Whether the inbound connection is enabled. Required.
-        port (int): The port number for the inbound connection. Required.
-        protocol (str): The protocol for the inbound connection. Required.
-        settings (Settings): The settings for the inbound connection. Required.
-        stream_settings (StreamSettings): The stream settings for the inbound connection. Optional.
-        sniffing (Sniffing): The sniffing settings for the inbound connection. Required.
-        listen (str): The listen address for the inbound connection. Optional.
-        remark (str): The remark for the inbound connection. Optional.
-        id (int): The ID of the inbound connection. Optional.
-        up (int): The up value for the inbound connection. Optional.
-        down (int): The down value for the inbound connection. Optional.
-        total (int): The total value for the inbound connection. Optional.
-        expiry_time (int): The expiry time for the inbound connection. Optional.
-        client_stats (list[Client]): The client stats for the inbound connection. Optional.
-        tag (str): The tag for the inbound connection. Optional.
+        email (str): The email of the client. Required.
+        enable (bool): Whether the client is enabled. Required.
+        password (str): The password of the client. Optional.
+        id (int | str): The ID of the client. Required.
+        inbound_id (int | None): The ID of the inbound connection. Optional.
+        up (int): The upload speed of the client. Optional.
+        down (int): The download speed of the client. Optional.
+        expiry_time (int): The expiry time of the client. Optional.
+        total (int): The total amount of data transferred by the client. Optional.
+        reset (int): The time at which the client's data was last reset. Optional.
+        flow (str): The flow of the client. Optional.
+        method (str): The method (encryption cipher) used by the client. Optional.
+        uuid (str): The UUID of the client. Optional.
+        limit_ip (int): The limit of IPs for the client. Optional.
+        sub_id (str): The sub ID of the client. Optional.
+        tg_id (str): The Telegram ID of the client. Optional.
+        total_gb (int): The total amount of data transferred by the client in GB. Optional.
     """
 
+    email: str
     enable: bool
-    port: int
-    protocol: str
-    settings: Settings
-    stream_settings: StreamSettings | str = Field(  # type: ignore
-        default="", alias=InboundFields.STREAM_SETTINGS
-    )
-    sniffing: Sniffing
+    id: int | str | None = Field(default=None)
+    password: str = Field(default="")  # type: ignore
 
-    listen: str = ""
-    remark: str = ""
-    id: int = 0
+    inbound_id: int | None = Field(default=None, alias=ClientFields.INBOUND_ID)  # type: ignore
 
+    uuid: str | None = None
     up: int = 0
     down: int = 0
 
+    expiry_time: int = Field(default=0, alias=ClientFields.EXPIRY_TIME)  # type: ignore
+
     total: int = 0
+    reset: int | None = None
 
-    expiry_time: int = Field(default=0, alias=InboundFields.EXPIRY_TIME)  # type: ignore
-    client_stats: list[Client] | None = Field(  # type: ignore
-        default=[], alias=InboundFields.CLIENT_STATS
-    )
-
-    tag: str = ""
+    flow: str = ""
+    method: str = ""
+    limit_ip: int = Field(default=0, alias=ClientFields.LIMIT_IP)  # type: ignore
+    sub_id: str = Field(default="", alias=ClientFields.SUB_ID)  # type: ignore
+    tg_id: int | str | None = Field(default="", alias=ClientFields.TG_ID)  # type: ignore
+    total_gb: int = Field(default=0, alias=ClientFields.TOTAL_GB)  # type: ignore
 
     model_config = ConfigDict(
         populate_by_name=True,
     )
-
-    @field_validator("stream_settings")
-    def validate_stream_settings(  # pylint: disable=no-self-argument
-        cls, value: StreamSettings | str
-    ) -> StreamSettings | str:
-        """Validates the stream settings field taking into account that it can be either a
-        dictionary with the StreamSettings model, JSON string or an empty string.
-        If the value is a string, it will try to parse it as JSON and create a StreamSettings,
-        otherwise it will return the value as is (empty string).
-
-        Args:
-            value (StreamSettings | str): The value to validate.
-
-        Returns:
-            StreamSettings | str: The validated value.
-        """
-        if isinstance(value, dict):
-            return StreamSettings(**value)
-        if isinstance(value, str):
-            try:
-                data = json.loads(value)
-                return StreamSettings(**data)
-            except json.JSONDecodeError:
-                pass
-        return value
-
-    # pylint: disable=no-member, no-self-argument
-    def to_json(self) -> dict[str, Any]:
-        """Converts the Inbound instance to a JSON-compatible dictionary for the XUI API.
-
-        Returns:
-            dict[str, Any]: The JSON-compatible dictionary.
-        """
-
-        include = {
-            InboundFields.REMARK,
-            InboundFields.ENABLE,
-            InboundFields.LISTEN,
-            InboundFields.PORT,
-            InboundFields.PROTOCOL,
-            InboundFields.EXPIRY_TIME,
-        }
-
-        result = super().model_dump(by_alias=True)
-        result = {k: v for k, v in result.items() if k in include}
-        result.update(
-            {
-                InboundFields.SETTINGS: self.settings.model_dump_json(by_alias=True),
-                InboundFields.SNIFFING: self.sniffing.model_dump_json(by_alias=True),
-            }
-        )
-
-        # Handle stream_settings which can be either StreamSettings or str.
-        if isinstance(self.stream_settings, StreamSettings):
-            result[InboundFields.STREAM_SETTINGS] = self.stream_settings.model_dump_json(
-                by_alias=True
-            )
-        else:
-            result[InboundFields.STREAM_SETTINGS] = self.stream_settings
-
-        return result
